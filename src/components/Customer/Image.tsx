792a247cf5aec312905af340cdabd60307039d6b
shibin <shibin>
2025-09-24 17:48:49 +0530

    fix: remove unnecessary blank lines in Image component

--- src/components/Customer/Image.tsx
+++ src/components/Customer/Image.tsx
@@ -5,6 +5,8 @@ const Image = () => {
   return (
      <div className="flex-1 hidden md:flex   bg-gradient-to-b from-[#F3F3F3] to-[#E6E0DD]  items-center justify-center">
         <img
+
+        
           src={image}
           alt="Construction workers with booking schedule"
           className="object-contain max-w-full max-h-full"
